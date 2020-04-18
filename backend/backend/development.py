20bdc9d6620ce307fa434a362586f0b9fb5f9d12
Ankit Mhatre <ankitmhatre.mhatre9@gmail.com>
2020-04-19 00:30:55 +0530

    Added gitignore

--- backend/backend/development.py
+++ backend/backend/development.py
@@ -15,12 +15,12 @@ CORS_ORIGIN_WHITELIST = [
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'django_react',
-        'USER': 'kirito',
-        'PASSWORD': 'sharingan',
-        'HOST': '127.0.0.1',
-        'PORT': '3306'
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+      # 'USER': 'kirito',
+        #'PASSWORD': 'sharingan',
+       # 'HOST': '127.0.0.1',
+        #'PORT': '3306'
     }
 }
 
